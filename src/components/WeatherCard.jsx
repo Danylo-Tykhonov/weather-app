78d8ceed0fa7706f70ef32b69311fd57b0089903
Dan Tykhonov <maloydania@gmail.com>
2026-07-26 23:31:23 +0300

    weathercard css and ux improved

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,4 +1,5 @@
 import "./WeatherCard.css";
+import { WiStrongWind, WiHumidity, WiThermometer, WiWindy  } from 'react-icons/wi';
 
 export default function WeatherCard({weather, selectedDay, setSelectedDay}) {
     
@@ -11,39 +12,36 @@ export default function WeatherCard({weather, selectedDay, setSelectedDay}) {
         {weather.icon}
     </div>
 
-    <h2 className="card-city">
-        {weather.city}
-    </h2>
-
     <p className="card-temperature">
         {weather.temperature}°C
     </p>
 
-     <p className="card-feelsLike">
-        Feels Like: {weather.feelsLike}°C
-    </p>
+    <h2 className="card-city">
+        {weather.city}
+    </h2>
 
-    <p className="card-time">
-        {weather.time}
-    </p>
-
-    <p className="card-timezone">
-        {weather.timezone}
-    </p>
-
-    <p className="card-windSpeed">
-        Wind Speed: {weather.windSpeed}km/h
-    </p>
-
-     <p className="card-humidity">
-        Humidity: {weather.humidity}%
-    </p>
-
+    <div className="sub-info">
+        <div className="info-card">
+            <WiThermometer/>
+            <div>Feels Like</div>
+            <div>{weather.feelsLike}°C</div>
+        </div>
+        <div className="info-card">
+            <WiWindy/>
+            <div>Wind</div>
+            <div>{weather.windSpeed} km/h</div>
+        </div>
+        <div className="info-card">
+            <WiHumidity/>
+            <div>Humidity</div>
+            <div>{weather.humidity}%</div>
+        </div>
+    </div>
         <h3>Today's forecast</h3>
 
     <div className="hourly">
         {hourlyForecastInfo.map(hour => (
-    <div key={hour.fullTime}>
+    <div className="hourly-item" key={hour.fullTime}>
       <p>{hour.time}</p>
       <div>{hour.icon}</div>
       <p>{hour.temperature}°C</p>
@@ -54,13 +52,18 @@ export default function WeatherCard({weather, selectedDay, setSelectedDay}) {
 
     <div className="daily">
         {weather.dailyForecast.map(daily => (
-      <div className={`daily-time ${selectedDay === daily.time ? "active" : ""}`} key={daily.time} onClick={() => setSelectedDay(daily.time)}>
+      <div className={`daily-item ${selectedDay === daily.time ? "active" : ""}`}  key={daily.time}  onClick={() => setSelectedDay(daily.time)}>
       <p>{daily.weekDay}</p>
       <div>{daily.icon}</div>
       <p>{daily.temperatureMin}/{daily.temperatureMax}°C</p>
     </div>
   ))}
 </div>
+
+    <div className="card-footer">
+        <p>{weather.time}</p>
+        <p>{weather.timezone}</p>
+    </div>
+
 </div>
-    )
-}
+    )}    
